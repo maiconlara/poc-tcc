d0954384039097b742512bfa38138909a4fef88e
Maicon <maiconluan.lara@yahoo.com.br>
2023-11-11 15:20:02 -0300

    refactor: removing unused router push

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,30 +1,27 @@
-import {  Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { styles } from "./styles";
 
-
-
 export default function App() {
   const router = useRouter();
   const { bottom, top } = useSafeAreaInsets();
   return (
-
     <View style={styles.container}>
-    <View style={styles.titleContainer}>
-      <Text style={styles.title}>POC TCC</Text>
-    </View>
-    <TouchableOpacity
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>POC TCC</Text>
+      </View>
+      <TouchableOpacity
         activeOpacity={0.6}
         style={styles.button}
         onPress={() => {
-          router.push("/menu");
+          router.push("/");
         }}
       >
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
-    <Footer />
-  </View>
+    </View>
   );
 }
