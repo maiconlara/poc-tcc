d9ca22082f1dd3fe72fba0596294de499c902ecc
Maicon <maiconluan.lara@yahoo.com.br>
2023-11-11 17:20:30 -0300

    refactor: password type in input

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -21,6 +21,7 @@ export default function App() {
         ></TextInput>
         <TextInput
           placeholder="Senha"
+          secureTextEntry={true}
           style={styles.input}
           onChangeText={() => {}}
         ></TextInput>
