18fd80874e99554186e827b17adc91956a469610
Maicon <maiconluan.lara@yahoo.com.br>
2023-11-12 00:45:06 -0300

    feat: harvester select page

--- mobile/app/pages/harvesterSelect.tsx
+++ mobile/app/pages/harvesterSelect.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 
 import logo from "../../src/assets/logo.png";
 
+import { handleHarvester } from "../../src/utils/handleHarvester";
+
 import { styles } from "../styles";
 
 const harvesterSelect = () => {
+  const [harvesterId, setHarvesterId] = useState("");
   const router = useRouter();
   return (
     <View style={styles.container}>
@@ -18,13 +21,15 @@ const harvesterSelect = () => {
           placeholder="ID da colhedora"
           keyboardType="number-pad"
           style={styles.input}
-          onChangeText={() => {}}
+          onChangeText={(text) => {
+            setHarvesterId(text);
+          }}
         ></TextInput>
         <TouchableOpacity
           activeOpacity={0.6}
           style={styles.button}
           onPress={() => {
-            router.push("/");
+            handleHarvester(harvesterId);
           }}
         >
           <Text style={styles.buttonText}>Selecionar</Text>
